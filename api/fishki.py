29dbc91187c66946d894c7983848ea12cb758674
Jakub Jachowicz <kubajachowicz39@gmail.com>
2024-06-25 21:41:12 +0200

    POST, UPDATE, DELETE methods done

--- api/fishki.py
+++ api/fishki.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
-from db import create_set, add_words, count_sets, set_exists
+from db import create_set, add_words, count_sets, set_exists, last_words_id, update_set, \
+    get_set, update_words, delete_set_by_id, delete_words_by_id, delete_all_words
 from api.utils import expect
 
 
@@ -13,13 +14,58 @@ def api_create_set():
         count = count_sets()
         while set_exists(count):
             count += 1
-        set_id = count
         name = expect(post_data.get('name'), str, 'name')
         lang_1 = expect(post_data.get('lang_1'), str, 'lang_1')
         lang_2 = expect(post_data.get('lang_2'), str, 'lang_2')
 
-        create_set(set_id, name, lang_1, lang_2)
+        create_set(count, name, lang_1, lang_2)
         return jsonify({'message': 'Set added...'}), 200
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+
+@fishki_api_v1.route('/update_set', methods=['PATCH'])
+def api_update_set():
+    body_data = request.get_json()
+    try:
+        set_id = expect(body_data.get('set_id'), int, 'set_id')
+        if not set_exists(set_id):
+            return jsonify({'error': f'Set with id {set_id} doesn\'t exist...'}), 404
+        set_data = get_set(set_id)
+
+        if not body_data.get('name'):
+            name = set_data['name']
+        else:
+            name = expect(body_data.get('name'), str, 'name')
+
+        if not body_data.get('lang_1'):
+            lang_1 = set_data['lang_1']
+        else:
+            lang_1 = expect(body_data.get('lang_1'), str, 'lang_1')
+
+        if not body_data.get('lang_2'):
+            lang_2 = set_data['lang_2']
+        else:
+            lang_2 = expect(body_data.get('lang_2'), str, 'lang_2')
+
+        update_set(set_id, name, lang_1, lang_2)
+        return jsonify({'message': f'Set with id {set_id} updated...'})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+
+@fishki_api_v1.route('/delete_set', methods=['DELETE'])
+def api_delete_set():
+    body_data = request.get_json()
+    try:
+        set_id = expect(body_data.get('set_id'), int, 'set_id')
+        if not set_exists(set_id):
+            return jsonify({'error': f'Set with id {set_id} doesn\'t exist...'}), 404
+
+        delete_set_by_id(set_id)
+        return jsonify({'message': 'Set has been deleted...'})
 
     except Exception as e:
         return jsonify({'error': str(e)}), 400
@@ -30,12 +76,66 @@ def api_add_words():
     post_data = request.get_json()
     try:
         set_id = expect(post_data.get('set_id'), int, 'set_id')
+        if not set_exists(set_id):
+            return jsonify({'error': f'Set with id {set_id} doesn\'t exist...'}), 404
         w1 = expect(post_data.get('word_1'), str, 'word_1')
         w2 = expect(post_data.get('word_2'), str, 'word_2')
-        words = [w1, w2]
+
+        id = last_words_id(set_id) + 1
+        words = [id, w1, w2]
 
         add_words(set_id, words)
         return jsonify({'message': f'Words added to set {set_id}...'})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+
+@fishki_api_v1.route('/update_words', methods=['PATCH'])
+def api_update_words():
+    body_data = request.get_json()
+    try:
+        set_id = expect(body_data.get('set_id'), int, 'set_id')
+        if not set_exists(set_id):
+            return jsonify({'error': f'Set with id {set_id} doesn\'t exist...'}), 404
+
+        words_id = expect(body_data.get('words_id'), int, 'words_id')
+        w1 = expect(body_data.get('word_1'), str, 'word_1')
+        w2 = expect(body_data.get('word_2'), str, 'word_2')
+
+        update_words(set_id, [words_id, w1, w2])
+        return jsonify({'message': 'Words updated...'})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+
+@fishki_api_v1.route('/delete_words', methods=['DELETE'])
+def api_delete_words():
+    body_data = request.get_json()
+    try:
+        set_id = expect(body_data.get('set_id'), int, 'set_id')
+        if not set_exists(set_id):
+            return jsonify({'error': f'Set with id {set_id} doesn\'t exist...'}), 404
+        words_id = expect(body_data.get('words_id'), int, 'words_id')
+
+        delete_words_by_id(set_id, words_id)
+        return jsonify({'message': 'Words deleted...'})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+
+@fishki_api_v1.route('/delete_all_words', methods=['DELETE'])
+def api_delete_all_words():
+    body_data = request.get_json()
+    try:
+        set_id = expect(body_data.get('set_id'), int, 'set_id')
+        if not set_exists(set_id):
+            return jsonify({'error': f'Set with id {set_id} doesn\'t exist...'}), 404
+
+        delete_all_words(set_id)
+        return jsonify({'message': 'Words from set deleted'})
 
     except Exception as e:
         return jsonify({'error': str(e)}), 400
--- db.py
+++ db.py
@@ -15,8 +15,23 @@ db = LocalProxy(get_db)
 
 
 def count_sets():
-    # print(db.get_collection('sets').estimated_document_count())
     return db.get_collection('sets').estimated_document_count()
+
+
+def get_set(set_id):
+    return db.sets.find_one({'set_id': set_id})
+
+
+def count_words_in_set(set_id):
+    return len(db.sets.find({'set_id': set_id})[0]['words'])
+
+
+def last_words_id(set_id):
+    words = db.sets.find({'set_id': set_id})[0]['words']
+    length = len(words)
+    if length == 0:
+        return 0
+    return words[length - 1][0]
 
 
 def set_exists(set_id):
@@ -30,5 +45,27 @@ def create_set(set_id, name, lang_1, lang_2):
     return db.sets.insert_one(set_doc)
 
 
+def delete_set_by_id(set_id):
+    return db.sets.delete_one({'set_id': set_id})
+
+
+def update_set(set_id, name, lang_1, lang_2):
+    return db.sets.update_one({'set_id': set_id}, {'$set': {'name': name, 'lang_1': lang_1, 'lang_2': lang_2}})
+
+
 def add_words(set_id, new_words):
     return db.sets.update_one({'set_id': set_id}, {'$push': {'words': new_words}})
+
+
+def delete_words_by_id(set_id, words_id):
+    return db.sets.update_one({'set_id': set_id}, {'$pull': {'words': {'$in': [words_id]}}})
+
+
+def delete_all_words(set_id):
+    return db.sets.update_one({'set_id': set_id}, {'$set': {'words': []}})
+
+
+def update_words(set_id, new_words):
+    return db.sets.update_one({'set_id': set_id},
+                              {'$set': {'words.$[xxx]': new_words}},
+                              array_filters=[{'xxx.0': new_words[0]}])
